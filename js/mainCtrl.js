3f7611ce91c2b861d8a1d7e0b8f2a3c85326d89a
Kurt Corbett <kurtcorbett@gmail.com>
2015-08-04 17:17:34 -0600

    first commit

---
+++ js/mainCtrl.js
@@ -0,0 +1,7 @@
+var app = angular.module('chatroom');
+
+app.controller('mainCtrl', function($scope, parseService){
+  
+  console.log('hello from controller');
+  
+})
---
+++ js/myService.js
@@ -0,0 +1,23 @@
+var app = angular.module('chatroom');
+
+app.service('parseService', function($http, $q){
+  
+  console.log('hello from service');
+   
+  this.getData = function() {
+
+  }
+  
+  
+
+});
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", 'http://reqr.es/api/products/5', true);
+// xhr.onload = function() {
+//   console.log(xhr.responseText)
+// };
+// xhr.send();
+
+// http://deckofcardsapi.com/api/deck/new/draw/?count=52
