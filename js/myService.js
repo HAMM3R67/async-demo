bf77e3b68a05b90fc9592b5e5f1319c46c748d07
Kurt Corbett <kurtcorbett@gmail.com>
2015-08-04 18:58:44 -0600

    before class

--- js/myService.js
+++ js/myService.js
@@ -3,21 +3,15 @@ var app = angular.module('chatroom');
 app.service('myService', function($http, $q){
   
   console.log('hello from service');
-   
-  this.getData = function() {
-
-  }
-  
-  
 
 });
 
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", 'http://reqr.es/api/products/5', true);
-// xhr.onload = function() {
-//   console.log(xhr.responseText)
-// };
-// xhr.send();
+var xhr = new XMLHttpRequest();
+xhr.open("GET", 'http://reqr.es/api/products/5', true);
+xhr.onload = function() {
+  console.log(xhr.responseText)
+};
+xhr.send();
 
 // http://deckofcardsapi.com/api/deck/new/draw/?count=52
